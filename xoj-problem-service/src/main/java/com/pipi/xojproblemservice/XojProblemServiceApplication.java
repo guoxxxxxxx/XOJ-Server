a3ab47a306dbda077d84d195d924f16119247849
guo_x <guo_x0315@163.com>
2024-08-28 17:08:19 +0800

    修改：各个微服务间的依赖

---
+++ xoj-problem-service/src/main/java/com/pipi/xojproblemservice/XojProblemServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pipi.xojproblemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XojProblemServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XojProblemServiceApplication.class, args);
+    }
+
+}
---
+++ xoj-problem-service/src/test/java/com/pipi/xojproblemservice/XojProblemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipi.xojproblemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XojProblemServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
