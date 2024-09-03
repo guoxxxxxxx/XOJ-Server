c087438c68a52d40e41f21fabfda0a235685f997
guo_x <guo_x0315@163.com>
2024-09-03 18:00:59 +0800

    修复：一些已知的bug

--- xoj-user-service/src/main/java/com/pipi/xojuserservice/filter/JwtAuthenticationTokenFilter.java
+++ xoj-user-service/src/main/java/com/pipi/xojuserservice/filter/JwtAuthenticationTokenFilter.java
@@ -19,6 +19,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
@@ -29,42 +30,48 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 @Component
 public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
 
+    @Value("${server.servlet.context-path}")
+    private String prefix_url;
+
+    private static final String[] AUTH_URLS = {"/user/login", "/user/register", "/user/registerSendAuthCode"};
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = JwtUtils.getToken(request);
-        if (!StringUtils.hasLength(token))
-//            throw new TokenErrorException("token 不存在");
+        List<String> authList = Arrays.asList(AUTH_URLS);
+        if (authList.contains(request.getRequestURI().substring(prefix_url.length()))){
             doFilter(request, response, filterChain);
+            return;
+        }
+        if (!StringUtils.hasLength(token))
+            throw new TokenErrorException("token 不存在");
         else{
-            if (SecurityContextHolder.getContext().getAuthentication() == null){
-                Claims claims;
-                try {
-                    claims = JwtUtils.parseJwt(token);
-                } catch (Exception e){
-                    log.error(e.toString());
-                    throw new TokenErrorException("token 解析异常");
-                }
-                String claimsSubject = claims.getSubject();
-                LoginUser loginUser = JSON.parseObject(claimsSubject, LoginUser.class);
-                if (JwtTokenUtils.isTokenValid(loginUser, token)){
-                    UsernamePasswordAuthenticationToken authenticationToken =
-                            new UsernamePasswordAuthenticationToken(loginUser
-                                    , null, loginUser.getAuthorities());
-                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                    SecurityContext context = SecurityContextHolder.createEmptyContext();
-                    context.setAuthentication(authenticationToken);
-                    SecurityContextHolder.setContext(context);
-                }
+            Claims claims;
+            try {
+                claims = JwtUtils.parseJwt(token);
+            } catch (Exception e){
+                log.error(e.toString());
+                throw new TokenErrorException("token 解析异常");
             }
-            else{
-                // TODO 待完成
-                return;
+            String claimsSubject = claims.getSubject();
+            LoginUser loginUser = JSON.parseObject(claimsSubject, LoginUser.class);
+            if (JwtTokenUtils.isTokenValid(loginUser, token)){
+                UsernamePasswordAuthenticationToken authenticationToken =
+                        new UsernamePasswordAuthenticationToken(loginUser
+                                , null, loginUser.getAuthorities());
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContext context = SecurityContextHolder.createEmptyContext();
+                context.setAuthentication(authenticationToken);
+                SecurityContextHolder.setContext(context);
             }
+            doFilter(request, response, filterChain);
         }
     }
 }
